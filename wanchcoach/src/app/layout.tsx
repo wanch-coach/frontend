8f25009dc36b1dd60d5e37ef3b71d9b1e452b9fc
najonghyun <i0364842@naver.com>
2024-06-12 19:21:10 +0900

    WC-708/feat:로그인 및 회원가입 레이아웃 구현

--- wanchcoach/src/app/layout.tsx
+++ wanchcoach/src/app/layout.tsx
@@ -1,21 +1,29 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ReactNode } from "react";
+import localFont from "next/font/local";
+import "../../public/fonts/style.css";
 
 export const metadata: Metadata = {
   title: "완치코치",
   description: "진료와 처방전 관리를 한번에",
   icons: {
-    icon: "favicon.ico",
+    icon: "/logo.png",
   },
 };
+
+const myFont = localFont({
+  src: "../../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
-    <html>
+    <html className={myFont.className}>
       <body>{children}</body>
     </html>
   );
