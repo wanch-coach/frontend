feefba7034b9f8cef2690becdd923a826e74cb28
najonghyun <i0364842@naver.com>
2024-06-27 23:00:58 +0900

    feat: 약검색페이지 - 검색바 구현

--- wanchcoach/src/app/(route)/mainpage/drug/page.tsx
+++ wanchcoach/src/app/(route)/mainpage/drug/page.tsx
@@ -1,3 +1,54 @@
+"use client";
+
+import styles from "./drug.module.css";
+import { useState, ChangeEvent } from "react";
+import { IoMdSearch } from "react-icons/io";
+
 export default function Drug() {
-  return <div>약 검색 페이지 입니다.</div>;
+  const [searchValue, setSearchValue] = useState("");
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+  const handleSearchSubmit = () => {
+    /* 약 검색하는 API 호출 해야함! */
+  };
+  return (
+    <div className={styles.body_container}>
+      <DrugSearchBox
+        placeholder="검색어 입력"
+        searchValue={searchValue}
+        handleSearchChange={handleSearchChange}
+        onClick={handleSearchSubmit}
+      />
+      <div className={styles.drug_list_container}></div>
+    </div>
+  );
+}
+
+interface DrugSearchBoxProps {
+  placeholder: string;
+  searchValue: string;
+  handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onClick: () => void;
+}
+function DrugSearchBox({
+  placeholder,
+  searchValue,
+  handleSearchChange,
+  onClick,
+}: DrugSearchBoxProps) {
+  return (
+    <div className={styles.drug_search_box}>
+      <input
+        className={styles.drug_search_input}
+        type="text"
+        placeholder={placeholder}
+        value={searchValue}
+        onChange={handleSearchChange}
+      />
+      <button className={styles.drug_search_button} onClick={onClick}>
+        <IoMdSearch size={"25px"} color="#757575" />
+      </button>
+    </div>
+  );
 }
