9f3a3b063915fca13f775c31bea5010b5b063422
najonghyun <i0364842@naver.com>
2024-06-30 00:35:20 +0900

    feat: 1차 테스트 준비

--- wanchcoach/next.config.mjs
+++ wanchcoach/next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  env: {
-    NEXT_PUBLIC_NCP_CLIENT_ID: process.env.NEXT_PUBLIC_NCP_CLIENT_ID,
-  },
   reactStrictMode: false,
 };
 
