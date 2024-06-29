6621e291e84a69aecc94450cb695bc96888f5eed
parkeg1223 <72430547+parkeg1223@users.noreply.github.com>
2024-06-29 22:44:17 +0900

    Update next.config.mjs

--- wanchcoach/next.config.mjs
+++ wanchcoach/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  env: {
+    NEXT_PUBLIC_NCP_CLIENT_ID: process.env.NEXT_PUBLIC_NCP_CLIENT_ID,
+  },
 };
 
 export default nextConfig;
